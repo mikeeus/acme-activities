2a53bfdb9aa72ba3b5445417d88e01eafda728e0
Mikias Abera <itsmikias@gmail.com>
2019-01-13 12:33:30 -0500

    Create e2e spec for registration form with validations

--- apps/activities-e2e/src/integration/app.spec.ts
+++ apps/activities-e2e/src/integration/app.spec.ts
@@ -1,9 +1,34 @@
-import { getGreeting } from '../support/app.po';
+import { getHeader, fillFirstName, getFormControlErrors, fillLastName, fillEmail, selectAcitivity, fillComments, nextStep, submitForm } from '../support/app.po';
 
 describe('Hello Nx', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to activities!');
+    getHeader().contains('Sign Up For ACME Activities!');
+
+    fillFirstName('m')
+    getFormControlErrors('First Name').contains('First Name must be at least 2 characters')
+    fillFirstName('{backspace}')
+    getFormControlErrors('First Name').contains('First Name is required')
+    fillFirstName('Mikias')
+
+    fillLastName('a')
+    getFormControlErrors('Last Name').contains('Last Name must be at least 2 characters')
+    fillLastName('{backspace}')
+    getFormControlErrors('Last Name').contains('Last Name is required')
+    fillLastName('Abera{enter}')
+
+    fillEmail('e')
+    getFormControlErrors('Email').contains('Email must be a valid email')
+    fillEmail('{backspace}')
+    getFormControlErrors('Email').contains('Email is required')
+    fillEmail('mikias@email.com{enter}')
+
+    selectAcitivity()
+    nextStep()
+    fillComments()
+    submitForm()
+
+    getHeader().contains('Registrations')
   });
 });
--- apps/activities-e2e/src/support/app.po.ts
+++ apps/activities-e2e/src/support/app.po.ts
@@ -1,1 +1,40 @@
-export const getGreeting = () => cy.get('h1');
+export const getHeader = () =>
+  cy.get('h1');
+
+export const getFormControl = (label) =>
+  cy.get(`acme-widgets-form-control[label='${label}']`)
+
+export const fillFirstName = (firstName='Mikias') =>
+  cy.get('input[formcontrolname=firstName]')
+    .type(firstName)
+
+export const getFormControlErrors = (label) =>
+  getFormControl(label).find('.errors small')
+
+export const fillLastName = (lastName='Abera') =>
+  cy.get('input[formcontrolname=lastName]')
+    .type(lastName)
+
+export const fillName = () =>
+  cy.get('input[formcontrolname=firstName]')
+    .type('Mikias')
+    .get('input[formcontrolname=lastName]')
+    .type('Abera{enter}')
+
+export const fillEmail = (email='mikias@email.com') =>
+  cy.get('input[formcontrolname=email]')
+    .type(email)
+    
+export const selectAcitivity = () =>
+  cy.get('select[formcontrolname=activity]')
+    .select('Initial Coin Offering')
+      
+export const nextStep = () =>
+  cy.get('button.next').click()
+      
+export const fillComments = () => 
+  cy.get('textarea[formcontrolname=comments]')
+    .type('Test Comments{enter}')
+      
+export const submitForm = () =>
+  cy.get('button.submit').click()
