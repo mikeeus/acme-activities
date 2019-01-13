a699ff12a3899f222c7099e571002e56983ae379
Mikias Abera <itsmikias@gmail.com>
2019-01-12 22:41:04 -0500

    Create basic state-machine and convert registration to multi-step form

--- libs/registrations/src/lib/registration-form/registration-form.component.ts
+++ libs/registrations/src/lib/registration-form/registration-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { Activity } from '@acme-widgets/models';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Observable } from 'rxjs';
+import { map, merge, withLatestFrom, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'acme-widgets-registration-form',
@@ -8,32 +11,82 @@ import { Activity } from '@acme-widgets/models';
   <form [formGroup]="form">
     <h2>Sign Up For ACME Activities!</h2>
   
-    <acme-widgets-form-control label="First Name" [control]="firstName">
-      <input formControlName="firstName" type="text" class="form-control" required>
-    </acme-widgets-form-control>
+    <div class="step" *ngIf="(step | async)?.name === 'name'">
+      <acme-widgets-form-control
+        label="First Name"
+        [control]="firstName">
+        <input
+          formControlName="firstName"
+          type="text"
+          class="form-control"
+          required>
+      </acme-widgets-form-control>
+    
+      <acme-widgets-form-control
+        label="Last Name"
+        [control]="lastName">
+        <input
+          formControlName="lastName"
+          type="text"
+          class="form-control"
+          required>
+      </acme-widgets-form-control>
+    </div>
+
+    <div class="step" *ngIf="(step | async)?.name === 'email'">
+      <acme-widgets-form-control
+        label="Email"
+        [control]="email">
+        <input
+          formControlName="email"
+          type="text"
+          class="form-control"
+          required>
+      </acme-widgets-form-control>
+    </div>
   
-    <acme-widgets-form-control label="Last Name" [control]="lastName">
-      <input formControlName="lastName" type="text" class="form-control" required>
-    </acme-widgets-form-control>
+    <div class="step" *ngIf="(step | async)?.name === 'activity'">
+      <acme-widgets-form-control
+        label="Acitivity"
+        [control]="activity">
+        <select formControlName="activity">
+          <option *ngFor="let option of activities" value="{{option.name}}">
+            {{option.name}}
+          </option>
+        </select>
+      </acme-widgets-form-control>
+    </div>
   
-    <acme-widgets-form-control label="Email" [control]="email">
-      <input formControlName="email" type="text" class="form-control" required>
-    </acme-widgets-form-control>
-  
-    <acme-widgets-form-control label="Acitivity" [control]="activity">
-      <select formControlName="activity">
-        <option *ngFor="let option of activities" value="{{option.name}}">
-          {{option.name}}
-        </option>
-      </select>
-    </acme-widgets-form-control>
-  
-    <acme-widgets-form-control label="Comments" [control]="comments">
-      <textarea formControlName="comments" type="text" class="form-control"></textarea>
-    </acme-widgets-form-control>
-  
+    <div class="step" *ngIf="(step | async)?.name === 'comments'">
+      <acme-widgets-form-control label="Comments" [control]="comments">
+        <textarea
+          formControlName="comments"
+          type="text"
+          class="form-control"
+          rows="10">
+        </textarea>
+      </acme-widgets-form-control>
+    </div>
+
     <div class="controls">
       <button
+        type="button"
+        *ngIf="(step | async)?.prev"
+        (click)="prevStep()">
+        Back
+      </button>
+
+      <button
+        type="button"
+        *ngIf="(step | async)?.next !== 'done'"
+        [disabled]="!(stepValid | async)"
+        (click)="nextStep()"
+        style="margin-left: auto;">
+        Next
+      </button>
+
+      <button
+        *ngIf="(step | async)?.next === 'done'"
         [disabled]="form.invalid"
         (click)="submitForm(); $event.preventDefault()">
         Submit
@@ -46,7 +99,9 @@ import { Activity } from '@acme-widgets/models';
 export class RegistrationFormComponent implements OnInit {
   @Output() submit = new EventEmitter<any>();
   @Input() activities: Activity[];
-
+  step: BehaviorSubject<RegistrationStep>;
+  stepValid: Observable<boolean>;
+  steps;
   form: FormGroup;
 
   constructor(private fb: FormBuilder) { }
@@ -58,7 +113,46 @@ export class RegistrationFormComponent implements OnInit {
       email: ['mikias@email.com', [Validators.required, Validators.email]],
       activity: ['Initial Coin Offering', [Validators.required, Validators.minLength(2)]],
       comments: ['Test'],
+    });
+
+    this.step = new BehaviorSubject({
+      name: 'name',
+      controls: [this.firstName, this.lastName],
+      next: 'email',
+      prev: null
     })
+
+    this.steps = {
+      name: {
+        name: 'name',
+        controls: [this.firstName, this.lastName],
+        next: 'email'
+      },
+      email: {
+        name: 'email',
+        controls: [this.email],
+        prev: 'name',
+        next: 'activity'
+      },
+      activity: {
+        name: 'activity',
+        controls: [this.activity],
+        prev: 'email',
+        next: 'comments'
+      },
+      comments: {
+        name: 'comments',
+        controls: [],
+        prev: 'activity',
+        next: 'done'
+      }
+    }
+
+    this.stepValid = this.form.valueChanges.pipe(
+      withLatestFrom(this.step),
+      map(([_formValue, step]) => this.validate(step.controls))
+    )
+    this.stepValid.subscribe(val => console.log('stepValid: ', val))
   }
 
   get firstName() { return this.form.get('firstName'); }
@@ -70,4 +164,26 @@ export class RegistrationFormComponent implements OnInit {
   submitForm() {
     this.submit.emit(this.form.value)
   }
+
+  validate(controls: AbstractControl[]): boolean {
+    console.log(controls)
+    return controls.length === 0 ||
+      controls.map((c: AbstractControl) => c.valid)
+              .indexOf(false) === -1;
+  }
+
+  nextStep() {
+    this.step.next(this.steps[this.step.value.next])
+  }
+
+  prevStep() {
+    this.step.next(this.steps[this.step.value.prev])
+  }
+}
+
+export interface RegistrationStep {
+  name: string;
+  prev: string;
+  next: string;
+  controls: AbstractControl[]
 }
